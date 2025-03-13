da6627ddaeac616c118a4309c19aea5ee3182e5a
arbaz798 <arbazmasood87@gmail.com>
2025-03-13 09:38:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let users = [];
 let events = [];
 let reminderTimers = {};
 function generateId() {
-  return Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36).substr(2, 9 );
 }
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
